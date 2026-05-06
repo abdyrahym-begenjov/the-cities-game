db3be14b3bcfaf32cb48929bc9c5d8f6eedfb5c8
Abdyrahym Begenjov <abdurahymbegencev@gmail.com>
2026-05-06 19:22:28 +0500

    Version 1.1
    v1.1: Add propython module, game menu, and scoring fix

--- The Cities Game.py
+++ The Cities Game.py
@@ -1,8 +1,14 @@
 from random import choice
-from json import load
+from propython import pyread
+from time import sleep
 
-with open('cities.json', 'r', encoding='utf-8') as i:
-    cities_list=load(i)
+print('The Cities Game')
+print('Creator: Abdyrahym Begenjov (GitHub: abdyrahym-begenjov)')
+start=input('Enter to start game: ')
+print('Loading...')
+sleep(2)
+
+cities_list=pyread('cities.json')
 
 word=choice(cities_list)
 heart=3
@@ -15,33 +21,36 @@ print(f'{number}) {word}')
 while True:
     city=word
     word=input(f'You have {heart} hearts. Enter the word: ')
-    if word=='':
-        word='Error!!!'
     word=word.title().strip()
-    if word[0]==city[-1].upper() and word in cities_list and word not in cities_set:
+    if heart==1:
+        print('Game Over!!!')
+        print(f'You received {points}')
+        match points:
+            case n if n>=60:
+                print('Absolute Champion!!! 🏆')
+            case n if n>=50:
+                print('⭐⭐⭐⭐⭐')
+            case n if n>=40:
+                print('⭐⭐⭐⭐')
+            case n if n>=30:
+                print('⭐⭐⭐')
+            case n if n>=20:
+                print('⭐⭐')
+            case n if n>=10:
+                print('⭐')
+            case _:
+                print('Loser!!!')
+        break
+    elif word=='':
+        print('You must enter the city!!!')
+        print('Error!!!')
+        heart-=1
+        word=city
+    elif word[0]==city[-1].upper() and word in cities_list and word not in cities_set:
         number+=1
         print(f'{number}) {word}')
         points+=1
         cities_set.add(word)
-    elif heart==1:
-        print('Game Over!!!')
-        print(f'You received {points}')
-        match points:
-            case n if n>=10:
-                print('⭐')
-            case n if n>=20:
-                print('⭐⭐')
-            case n if n>=30:
-                print('⭐⭐⭐')
-            case n if n>=40:
-                print('⭐⭐⭐⭐')
-            case n if n>=50:
-                print('⭐⭐⭐⭐⭐')
-            case n if n>=60:
-                print('Absolute Champion!!! 🏆')
-            case _:
-                print('Loser!!!')
-        break
     else:
         if word in cities_set:
             print('This word has already been used.')
@@ -49,3 +58,5 @@ while True:
         print(word)
         heart-=1
         word=word[0]
+
+end=input('Enter to quit: ')
---
+++ propython.py
@@ -0,0 +1,36 @@
+import json
+import pickle
+def pyread(filename):
+    try:
+        match filename:
+            case filename if filename.endswith('.txt'):
+                with open(filename, 'r', encoding='utf-8') as file:
+                    return file.read().strip()
+            case filename if filename.endswith('.json'):
+                with open(filename, 'r', encoding='utf-8') as file:
+                    return json.load(file)
+            case filename if filename.endswith('.pickle'):
+                with open(filename, 'rb') as file:
+                    return pickle.load(file)
+            case _:
+                print('Error!!!')
+    except FileNotFoundError:
+        print('File is not found!!!')
+
+
+def pywrite(filename, value):
+    try:
+        match filename:
+            case filename if filename.endswith('.txt'):
+                with open(filename, 'w', encoding='utf-8') as file:
+                    file.write(value)
+            case filename if filename.endswith('.json'):
+                with open(filename, 'w', encoding='utf-8') as file:
+                    json.dump(value, file)
+            case filename if filename.endswith('.pickle'):
+                with open(filename, 'wb') as file:
+                    pickle.dump(value, file)
+            case _:
+                print('Error!!!')
+    except FileNotFoundError:
+        print('File is not found!!!')
